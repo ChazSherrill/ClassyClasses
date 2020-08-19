28db144d413bdc9d4c807db52308f24631f89b55
Chaz Sherrill <chazsherrill@icloud.com>
2020-08-19 13:13:56 -0500

    be classy

---
+++ Thebeginningsofaclass.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace My_Second_Csharp_Program
+{
+    class Program
+    {
+        class Person
+        {
+            int age;
+            string name;
+            public void SayHi()
+            {
+                Console.WriteLine("hi");
+            }//The above is a class, with a method and some variables inside
+        }
+        static void Main(string[] args)
+        {
+            Person p1 = new Person();
+            p1.SayHi();//the . operator that is used to access and call the method of the object
+        }//the new operator instantiates an object and returns a reference to its location
+    }
+}
